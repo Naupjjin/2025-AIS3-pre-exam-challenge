fecc139e823b3f5a0d58fbe2f88c849b59a93855
naup96321 <naup96721@gmail.com>
2025-04-27 19:51:51 +0800

    edit class

--- MyGO-schedule-manager-beta/server/share/chal.cpp
+++ MyGO-schedule-manager-beta/server/share/chal.cpp
@@ -1,189 +1,173 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstring> 
+#include <cstring>
 #include <csignal>
 
-// g++ chal.cpp -o chal -s
-
 #define SCHEDULE_MAX 4
-#define SCHEDULE_SIZE schedule.capacity()
 
-std::vector<std::string> schedule;
+class ScheduleManager {
+private:
+    std::vector<std::string> schedule;
+    int ops_cnt = 0;
 
-void timeout_handler(int signum) {
-    std::cerr << "Time out !\n";
-    _exit(1);  
-}
-
-
-void init_proc(){
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
-    std::cin.rdbuf()->pubsetbuf(nullptr, 0);
-    std::cout.rdbuf()->pubsetbuf(nullptr, 0);
-
-    signal(SIGALRM, timeout_handler); 
-    alarm(60);
-    
-    puts("+======== beta ========+");
-    puts("| Band schedule system |");
-    puts("+======================+");
-    
-}
-
-void menu(){
-    puts("+======================+");
-    puts("| (1) add schedule     |");
-    puts("| (2) edit schedule    |");
-    puts("| (3) delete schedule  |");
-    puts("| (4) Show schedule    |");
-    puts("+======================+");
-    printf("< MyGO @ ScheduleManager $ > ");
-}
-
-int get_choice(){
-    int choice;
-    scanf("%d", &choice);
-    return choice;
-}
-
-std::string get_string(){
-    std::string MSG;
-    printf("Input item > ");
-    std::cin.ignore();
-    std::getline(std::cin, MSG);
-    
-    return MSG;
-}
-
-
-void add_scedule(){
-    if(SCHEDULE_SIZE > SCHEDULE_MAX){
-        puts("[!] schedule is full. Don't let Tomorin so tired...");
-        return;
+    static void timeout_handler(int signum) {
+        std::cerr << "Time out!\n";
+        _exit(1);
     }
-    schedule.emplace_back(get_string());
-    
-    puts("[+] add success!");
-    return;
-}
 
+    void init_proc() {
+        setvbuf(stdin, NULL, _IONBF, 0);
+        setvbuf(stdout, NULL, _IONBF, 0);
+        std::cin.rdbuf()->pubsetbuf(nullptr, 0);
+        std::cout.rdbuf()->pubsetbuf(nullptr, 0);
 
-void edit_schedule(int index){
-    
-    if(index > SCHEDULE_SIZE || index < 0){
-        puts("[x] schedule is not exist");
-        return;
+        signal(SIGALRM, timeout_handler);
+        alarm(60);
+
+        puts("+======== beta ========+");
+        puts("| Band schedule system |");
+        puts("+======================+");
     }
-    
-    std::cin.ignore();
-    printf("Edit item > ");
-    std::getline(std::cin, schedule[index]);
-    
-    puts("[+] Edit success!");
-    return;
-}
 
-void delete_schedule(int index){
-    if(index > SCHEDULE_SIZE || index < 0){
-        puts("[x] schedule is not exist");
-        return;
+    void menu() {
+        puts("+======================+");
+        puts("| (1) add schedule     |");
+        puts("| (2) edit schedule    |");
+        puts("| (3) delete schedule  |");
+        puts("| (4) show schedule    |");
+        puts("+======================+");
+        printf("< MyGO @ ScheduleManager $ > ");
     }
-    
-    schedule.erase(schedule.begin() + index);
-    puts("[-] delete success!");
-    return;
-}
 
-void show_schedule(int index){
-    if(index > SCHEDULE_SIZE || index < 0){
-        puts("[x] schedule is not exist");
-        return;
+    int get_choice() {
+        int choice;
+        scanf("%d", &choice);
+        return choice;
     }
-    
-    std::cout << "Index [" << index << "] is " << schedule[index] << std::endl;
-    return;
-}
 
-int get_index(){
-    int index;
-    printf("Index > ");
-    scanf("%d", &index);
-    
-    return index;
-}
-
-void login(){
-    char username[0x10];  
-    char password[0x10]; 
-    
-    printf("Username > ");
-    scanf("%15s", username);
-
-    printf("Password > ");
-    scanf("%15s", password);
-    
-    if (strcmp(username, "MyGO!!!!!") == 0 && strcmp(password, "TomorinIsCute") == 0){
-        puts("\033[34m");
-        puts("=========================================");
-        puts("                  ____    _____      ");  
-        puts(" /'\\_/`\\         /\\  _`\\ /\\  __`\\    ");  
-        puts("/\\      \\  __  __\\ \\ \\L\\_\\ \\ \\/\\ \\   ");  
-        puts("\\ \\ \\__\\ \\/\\ \\/\\ \\\\ \\ \\L_L\\ \\ \\ \\ \\  ");  
-        puts(" \\ \\ \\_/\\ \\ \\ \\_\\ \\\\ \\ \\/, \\ \\ \\_\\ \\ ");  
-        puts("  \\ \\_\\\\ \\_\\/`____ \\\\ \\____/\\ \\_____\\");  
-        puts("   \\/_/ \\/_/`/___/> \\\\/___/  \\/_____/");  
-        puts("               /\\___/                 ");  
-        puts("               \\/__/                  ");  
-        puts("=========================================");
-        puts("\033[0m");
-        puts("[!] This is a system that can manage your band schedule.");
-        return;
-    } else {
-        puts("[x] Verify Failed");
-        exit(0);
+    std::string get_string(const char* prompt) {
+        std::string input;
+        printf("%s", prompt);
+        std::cin.ignore();
+        std::getline(std::cin, input);
+        return input;
     }
-}
 
-int main()
-{
-    init_proc();
-    
-    int choice;
-    int index;
-    int ops_cnt = 0 ;
-    
-    login();
-    
-    while(1){
-        menu();
-        choice = get_choice();
-        ops_cnt++;
-        if (ops_cnt < 12) {
-            if(choice == 1){
-                add_scedule();
-                
-            } else if (choice != 2 && choice != 3 && choice != 4) {
-                printf("[!] Thanks ~ ");
-                break;
-            } else {
-                index = get_index();
-                if (choice == 2){
-                    edit_schedule(index);
-                    
-                } else if (choice == 3){
-                    delete_schedule(index);
-                    
-                } else if (choice == 4){
-                    show_schedule(index);
-                
-                }
-            }
+    int get_index() {
+        int index;
+        printf("Index > ");
+        scanf("%d", &index);
+        return index;
+    }
+
+    void login() {
+        char username[0x10];
+        char password[0x10];
+
+        printf("Username > ");
+        scanf("%15s", username);
+
+        printf("Password > ");
+        scanf("%15s", password);
+
+        if (strcmp(username, "MyGO!!!!!") == 0 && strcmp(password, "TomorinIsCute") == 0) {
+            puts("\033[34m");
+            puts("=========================================");
+            puts("                  ____    _____      ");
+            puts(" /'\\_/\\         /\\  _\\ /\\  __\\    ");
+            puts("/\\      \\  __  __\\ \\ \\L\\_\\ \\ \\/\\ \\   ");
+            puts("\\ \\ \\__\\ \\/\\ \\/\\ \\\\ \\ \\L_L\\ \\ \\ \\ \\  ");
+            puts(" \\ \\ \\_/\\ \\ \\ \\_\\ \\\\ \\ \\/, \\ \\ \\_\\ \\ ");
+            puts("  \\ \\_\\\\ \\_\\/____ \\\\ \\____/\\ \\_____\\");
+            puts("   \\/_/ \\/_//___/> \\\\/___/  \\/_____/");
+            puts("               /\\___/                 ");
+            puts("               \\/__/                  ");
+            puts("=========================================");
+            puts("\033[0m");
+            puts("[!] This is a system that can manage your band schedule.");
         } else {
-            puts("[x] Too many operation !");
+            puts("[x] Verify Failed");
             exit(0);
         }
     }
+
+public:
+    ScheduleManager() {
+        init_proc();
+        login();
+    }
+
+    void add_schedule() {
+        if (schedule.capacity() >= SCHEDULE_MAX) {
+            puts("[!] Schedule is full. Don't let Tomorin so tired...");
+            return;
+        }
+        schedule.emplace_back(get_string("Input item > "));
+        puts("[+] Add success!");
+    }
+
+    void edit_schedule(int index) {
+        if (index < 0 || index >= (int)schedule.capacity()) {
+            puts("[x] Schedule does not exist");
+            return;
+        }
+        std::cin.ignore();
+        printf("Edit item > ");
+        std::getline(std::cin, schedule[index]);
+        puts("[+] Edit success!");
+    }
+
+    void delete_schedule(int index) {
+        if (index < 0 || index >= (int)schedule.capacity()) {
+            puts("[x] Schedule does not exist");
+            return;
+        }
+        schedule.erase(schedule.begin() + index);
+        puts("[-] Delete success!");
+    }
+
+    void show_schedule(int index) {
+        if (index < 0 || index >= (int)schedule.capacity()) {
+            puts("[x] Schedule does not exist");
+            return;
+        }
+        std::cout << "Index [" << index << "] is " << schedule[index] << std::endl;
+    }
+
+    void run() {
+        while (true) {
+            menu();
+            int choice = get_choice();
+            ops_cnt++;
+            if (ops_cnt >= 12) {
+                puts("[x] Too many operations!");
+                exit(0);
+            }
+
+            switch (choice) {
+                case 1:
+                    add_schedule();
+                    break;
+                case 2:
+                    edit_schedule(get_index());
+                    break;
+                case 3:
+                    delete_schedule(get_index());
+                    break;
+                case 4:
+                    show_schedule(get_index());
+                    break;
+                default:
+                    printf("[!] Thanks ~ ");
+                    return;
+            }
+        }
+    }
+};
+
+int main() {
+    ScheduleManager manager;
+    manager.run();
     return 0;
 }
